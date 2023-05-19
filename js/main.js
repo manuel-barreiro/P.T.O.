20406295251f548d22e268c011fc99ba92195c15
Manuel Barreiro <manuel.barreiro@ing.austral.edu.ar>
2023-05-19 15:36:03 -0300

    adapted

--- js/main.js
+++ js/main.js
@@ -24,6 +24,31 @@ window.addEventListener('scroll', function() {
     }
   });
 
+// hamburger menu
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+
+}))
+
+// Click/Touch Logo 
+
+const productCard = document.getElementById('branding');
+
+productCard.addEventListener('click', function() {
+  window.location.href = 'index.html';
+  console.log("hice click");
+});
+
 // scrollIntoView pantalla > 1024px
 
 document.querySelector('.logoPto').addEventListener('click', () => {
